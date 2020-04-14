73dc9a916db4a8e6b01e67a65d3c7c89418253c9
jiaan.cui <478525444@qq.com>
2020-04-14 22:20:35 +0800

    change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import PriceList from './components/PriceList'
+
+const items = [
+  {
+    "id": 1,
+    "title": "去云南旅游",
+    "price": 200,
+    "date": "2018-09-10",
+    "category": {
+      "id": 1,
+      "name": "旅行",
+      "type": "outcome",
+      "iconName": "ios-plane"
+    }
+  },
+  {
+    "id": 2,
+    "title": "去云南旅游",
+    "price": 400,
+    "date": "2018-09-10",
+    "category": {
+      "id": 1,
+      "name": "旅行",
+      "type": "outcome",
+      "iconName": "ios-plane"
+    }
+  }
+]
 
 function App() {
   return (
@@ -19,6 +49,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <PriceList
+        items={items}
+        onModifyItem={(item) => {alert(item.id)}}
+        onDeleteItem={(item) => {alert(item.id)}}
+      />
     </div>
   );
 }
